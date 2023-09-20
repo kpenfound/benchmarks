59145523dd835dbcd1478fa4e701dc94abfbb5b2
kpenfound <kyle@dagger.io>
2023-09-19 22:36:48 -0400

    sleepy pipelien

--- hello/main.go
+++ hello/main.go
@@ -20,6 +20,7 @@ func main() {
 		From("alpine@sha256:c5c5fda71656f28e49ac9c5416b3643eaa6a108a8093151d6d1afc9463be8e33").
 		WithExec([]string{"apk", "add", "curl"}).
 		WithExec([]string{"curl", "https://dagger.io"}).
+		WithExec([]string{"sleep", "10"}).
 		Stdout(ctx)
 
 	if err != nil {
