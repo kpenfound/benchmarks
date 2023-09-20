e9a6f2ba1b6bca4bee40f3ca3e8827b9431d5f31
kpenfound <kyle@dagger.io>
2023-09-19 22:15:42 -0400

    add engine output

--- hello/main.go
+++ hello/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 	"dagger.io/dagger"
 )
 
 func main() {
 	ctx := context.Background()
-	client, err := dagger.Connect(ctx)
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
 	if err != nil {
 		panic(err)
 	}
